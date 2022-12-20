1f999b25da1ffb1d01b23ec2a937041a1422134c
unknown <Obada-sh>
2022-12-20 12:21:46 -0800

    search controller

--- our_project/app/Http/Controllers/HomeController.php
+++ our_project/app/Http/Controllers/HomeController.php
@@ -44,7 +44,7 @@ class HomeController extends Controller
 
 
 
-          return response()->json([
+        return response()->json([
             'status' => true,
             'data' => $data
         ],200);
@@ -88,40 +88,9 @@ class HomeController extends Controller
 
 
 
-          return response()->json([
+        return response()->json([
             'status' => true,
             'data' => $data
         ],200);
     }
-
-
-
-    
-
-    public function search( $name)
-    {
-        $cats = DB::table('categories')->where('name' , 'Like' , '%'.$name.'%')->get();
-
-foreach ($cats as $cat)
-{
-    $exps = DB::table('experiences')->where('category_id' , 'Like' , '%'.$cat->id.'%')->get();
-    foreach ($exps as $exp)
-    {
-        $users = DB::table('users')->where('id' , 'Like' , '%'.$exp->user_id.'%')->get();
-        foreach ($users as $user)
-        {
-            var_dump($user->name,$user->id);
-        }
-    }
-
-
-}
-
-
-        // $ids = Category::where('name' , 'Like' , '%'.$name.'%')->select('id')->get();
-        // $ids2 = Experience::where('category_id' , 'Like' , $ids)->select('user_id')->get();
-        // return User::where('id' , 'Like' , $ids2 ) -> select('name')->get();
-        //return User::where('name' , 'Like' , '%'.$name.'%' ) -> select('name')->get();
-    }
-
 }
